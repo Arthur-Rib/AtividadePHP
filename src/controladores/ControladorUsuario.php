ad2ef257625092a8b8b058df3e1ba62aeca80d16
Arthur-Rib <arthurbrito2003@gmail.com>
2021-07-14 18:44:19 +0000

    renomear para novo padrão

--- src/controladores/ControladorUsuario.php
+++ src/controladores/ControladorUsuario.php
@@ -11,13 +11,13 @@ class ControladorUsuario
         $email = $_POST["email"];
         $password = $_POST["password"];
         if (!isset($username) || !isset($fullname) || !isset($email) || !isset($password)) {
-            require_once "app/views/signup/index.php";
+            require_once "src/telas/cadastro/index.php";
         } else {
             $usuario = new Usuario($username, $password, $fullname, $email);
             if (!is_bool($result)) {
-                require_once "app/views/login/index.php";
+                require_once "src/telas/login/index.php";
             } else {
-                require_once "app/views/signup/index.php";
+                require_once "src/telas/cadastro/index.php";
             }
         }
     }
@@ -27,14 +27,14 @@ class ControladorUsuario
         $email = $_POST["email"];
         $password = $_POST["password"];
         if (!isset($email) || !isset($password)) {
-            require_once "app/views/signup/index.php";
+            require_once "src/telas/cadastro/index.php";
         } else {
             $result = $this->userService->logIn($email, $password);
             if (!is_bool($result)) {
                 $_SESSION["loggedUser"] = array("id" => $result->getId(), "username" => $result->getUsername(), "email" => $result->getEmail());
-                require_once "app/views/home/index.php";
+                require_once "src/telas/home/index.php";
             } else {
-                require_once "app/views/login/index.php";
+                require_once "src/telas/login/index.php";
             }
         }
     }
